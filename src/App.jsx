f32c11a5e50457d517ba09a3da1d6bf61c42843a
Shranshikha Somkuwar <shranshikhasomkuwar@gmail.com>
2026-08-14 13:50:47 +0530

    Integrate application routes and authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-
-import ReportIssue from "./pages/ReportIssue";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
@@ -9,30 +10,115 @@ import HomeSections from "./components/HomeSections";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ReportIssue from "./pages/ReportIssue";
+import Dashboard from "./pages/Dashboard";
+import AdminDashboard from "./pages/AdminDashboard";
+
+import ProtectedRoute from "./components/ProtectedRoute";
 
 import "./App.css";
 
+
+/* =========================================================
+   HOME PAGE
+========================================================= */
+
 function Home() {
   return (
-    <>
+    <div className="app">
+
       <Navbar />
-      <Hero />
-      <HomeSections />
-    </>
+
+      <main>
+        <Hero />
+        <HomeSections />
+      </main>
+
+    </div>
   );
 }
+
+
+/* =========================================================
+   APP
+========================================================= */
 
 function App() {
   return (
     <BrowserRouter>
-     <Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/login" element={<Login />} />
-  <Route path="/register" element={<Register />} />
-  <Route path="/report-issue" element={<ReportIssue />} />
-</Routes>
+
+      <Routes>
+
+        {/* =================================================
+            PUBLIC ROUTES
+        ================================================= */}
+
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+
+
+        {/* =================================================
+            LOGGED-IN CITIZEN ROUTES
+        ================================================= */}
+
+        <Route
+          element={
+            <ProtectedRoute allowedRole="citizen" />
+          }
+        >
+
+          {/* REPORT ISSUE */}
+
+          <Route
+            path="/report-issue"
+            element={<ReportIssue />}
+          />
+
+          {/* CITIZEN DASHBOARD */}
+
+          <Route
+            path="/dashboard"
+            element={<Dashboard />}
+          />
+
+        </Route>
+
+
+        {/* =================================================
+            ADMIN ROUTES
+        ================================================= */}
+
+        <Route
+          element={
+            <ProtectedRoute allowedRole="admin" />
+          }
+        >
+
+          <Route
+            path="/admin"
+            element={<AdminDashboard />}
+          />
+
+        </Route>
+
+
+      </Routes>
+
     </BrowserRouter>
   );
 }
+
 
 export default App;
